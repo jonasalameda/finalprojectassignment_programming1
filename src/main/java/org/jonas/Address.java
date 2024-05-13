202708ac74c1e0d51bc1dfbb12fabc9e30dbbd96
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-13 13:32:02 -0400

    fix isPostalCodeValid in Address

--- src/main/java/org/jonas/Address.java
+++ src/main/java/org/jonas/Address.java
@@ -17,6 +17,7 @@ public class Address {
         this.city = city;
         this.province = province;
         this.country = country;
+        this.postalCode = isPostalCodeValid(postalCode) ? postalCode : null;
     }
 
     /**
@@ -24,11 +25,27 @@ public class Address {
      * if the length is 6, then it is CDCDCD
      * if the length is 7 then it is CDC DCD
      * where C is a character and D is a digit (num)
-     * @param postalCode
-     * @return if a postal code is valid or not
+     * @param postalCode postal code given in String, should length of 6 or 7
+     * @return if the postal code is valid or not
      */
-//    public static boolean isPostalCodeValid(String postalCode) {
-//        Character.isAlphabetic(postalCode.charAt(0)); //and it goes on
-//        Character.isDigit(postalCode.charAt(1)); // and so on
-//    }
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode.length() == 6) {
+            return Character.isAlphabetic(postalCode.charAt(0)) &&
+            Character.isDigit(postalCode.charAt(1)) &&
+            Character.isAlphabetic(postalCode.charAt(2)) &&
+            Character.isDigit(postalCode.charAt(3)) &&
+            Character.isAlphabetic(postalCode.charAt(4)) &&
+            Character.isDigit(postalCode.charAt(5));
+        }
+        if (postalCode.length() == 7) {
+            return Character.isAlphabetic(postalCode.charAt(0)) &&
+                    Character.isDigit(postalCode.charAt(1)) &&
+                    Character.isAlphabetic(postalCode.charAt(2)) &&
+                    Character.isWhitespace(postalCode.charAt(3)) &&
+                    Character.isDigit(postalCode.charAt(4)) &&
+                    Character.isAlphabetic(postalCode.charAt(5)) &&
+                    Character.isDigit(postalCode.charAt(6));
+        }
+        return false;
+    }
 }
