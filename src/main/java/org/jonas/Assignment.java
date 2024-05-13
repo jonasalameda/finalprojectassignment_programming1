b30acb7674033a63d13233edcf21bfbf73e95c41
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-13 14:23:09 -0400

    create Assignment class with fields

---
+++ src/main/java/org/jonas/Assignment.java
@@ -0,0 +1,13 @@
+package org.jonas;
+
+import java.util.ArrayList;
+
+public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private int maxScore;
+    private double assignmentAverage;
+    private ArrayList<Integer> scores;
+    private static int nextId = 1;
+}
