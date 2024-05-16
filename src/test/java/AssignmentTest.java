3c123f32108ada205d2f5a08dd73a4129038d14b
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-16 13:31:22 -0400

    add CourseTest unit testing

---
+++ src/test/java/AssignmentTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class AssignmentTest {
+    @Test
+    public void testcalcAssignmentAvg1() {
+    }
+
+    @Test
+    public void testIsAssignmentsTotalWeightValid() {
+    }
+}
