8ef2ddfdad2cdf162f14b9a07ceb84a093a57d09
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-16 13:29:45 -0400

    add AddressTest and AssignmentTest unit testing

---
+++ src/test/java/AddressTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class AddressTest {
+    @Test
+    public void testIsPostalCodeValid1() {
+
+    }
+    @Test
+    public void testIsPostalCodeValid2() {
+
+    }
+    @Test
+    public void testIsPostalCodeValid3() {
+
+    }
+    @Test
+    public void testIsPostalCodeValid4() {
+
+    }
+}
