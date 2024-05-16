3bf458426a1b4f26d8515ed526681c469aa2d095
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-16 13:11:57 -0400

    create Course equals, getters, setters and start methods

--- src/main/java/org/jonas/Course.java
+++ src/main/java/org/jonas/Course.java
@@ -1,7 +1,14 @@
 package org.jonas;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Course {
     private String courseId;
     private String courseName;
@@ -11,4 +18,32 @@ public class Course {
     private ArrayList<Student> students;
     private ArrayList<Double> finalScores;
     private static int nextId = 1;
+
+    public boolean isAssignmentWeightValid() {
+        return false;
+        //TODO: to be implemented
+    }
+
+    public boolean registerStudent(Student student) {
+        return false;
+        //TODO: to be implemented
+    }
+
+    public int[] calcStudentAverage() {
+        return new int[0];
+        //TODO: to be implemented
+    }
+
+    public boolean addAssignment(String assignment, double weight, int maxScore) {
+        return false;
+        //TODO: to be implemented
+    }
+
+    public void generateScore() {
+        //TODO: to be implemented
+    }
+
+    public void displayScores() {
+        //TODO: to be implemented
+    }
 }
