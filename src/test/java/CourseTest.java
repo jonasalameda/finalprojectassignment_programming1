b3663568d882a038664c0281107a8782a91170d9
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-26 18:25:00 -0400

    Add CourseTest.java unit testing

--- src/test/java/CourseTest.java
+++ src/test/java/CourseTest.java
@@ -1,7 +1,113 @@
+import org.jonas.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class CourseTest {
     @Test
     public void testCalcStudentAvg1() {
+        Student student = new Student("Dog", Gender.MALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Student student2 = new Student("Cat", Gender.FEMALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Student student3 = new Student("Bird", Gender.FEMALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Course course = new Course("Programming1", 4, new Department("CST"));
+        student.registerCourse(course);
+        student2.registerCourse(course);
+        student3.registerCourse(course);
+        course.addAssignment("Exam1", 0.2, 100);
+        course.addAssignment("Exam2", 0.2, 100);
+        course.addAssignment("FinalExam", 0.6, 100);
+
+        course.getAssignments().get(0).getScores().set(0, 80);
+        course.getAssignments().get(1).getScores().set(0, 80);
+        course.getAssignments().get(2).getScores().set(0, 80);
+
+        course.getAssignments().get(0).getScores().set(1, 80);
+        course.getAssignments().get(1).getScores().set(1, 80);
+        course.getAssignments().get(2).getScores().set(1, 80);
+
+        course.getAssignments().get(0).getScores().set(2, 80);
+        course.getAssignments().get(1).getScores().set(2, 80);
+        course.getAssignments().get(2).getScores().set(2, 80);
+
+        course.calcStudentsAverage();
+
+        ArrayList<Double> expectedResult = new ArrayList<>();
+        expectedResult.add(80.0);
+        expectedResult.add(80.0);
+        expectedResult.add(80.0);
+        ArrayList<Double> result = course.getFinalScores();
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testCalcStudentAvg2() {
+        Student student = new Student("Dog", Gender.MALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Student student2 = new Student("Cat", Gender.FEMALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Student student3 = new Student("Bird", Gender.FEMALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Course course = new Course("Programming1", 4, new Department("CST"));
+        student.registerCourse(course);
+        student2.registerCourse(course);
+        student3.registerCourse(course);
+        course.addAssignment("Exam1", 0.2, 100);
+        course.addAssignment("Exam2", 0.2, 100);
+        course.addAssignment("FinalExam", 0.6, 100);
+
+        course.getAssignments().get(0).getScores().set(0, 40);
+        course.getAssignments().get(1).getScores().set(0, 80);
+        course.getAssignments().get(2).getScores().set(0, 90);
+
+        course.getAssignments().get(0).getScores().set(1, 85);
+        course.getAssignments().get(1).getScores().set(1, 75);
+        course.getAssignments().get(2).getScores().set(1, 55);
+
+        course.getAssignments().get(0).getScores().set(2, 80);
+        course.getAssignments().get(1).getScores().set(2, 90);
+        course.getAssignments().get(2).getScores().set(2, 100);
+
+        course.calcStudentsAverage();
+
+        ArrayList<Double> expectedResult = new ArrayList<>();
+        expectedResult.add(78.0);
+        expectedResult.add(65.0);
+        expectedResult.add(94.0);
+        ArrayList<Double> result = course.getFinalScores();
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testCalcStudentAvg3() {
+        Student student = new Student("Dog", Gender.MALE, new Address(5, "x", "x", "x", "x", "H8N 3B3"), new Department("CST"));
+        Course course = new Course("Programming1", 4, new Department("CST"));
+        student.registerCourse(course);
+        course.addAssignment("Exam1", 0.2, 100);
+        course.addAssignment("Exam2", 0.2, 100);
+        course.addAssignment("FinalExam", 0.6, 100);
+
+        course.getAssignments().get(0).getScores().set(0, 40);
+        course.getAssignments().get(1).getScores().set(0, 80);
+        course.getAssignments().get(2).getScores().set(0, 90);
+
+        course.calcStudentsAverage();
+
+        ArrayList<Double> expectedResult = new ArrayList<>();
+        expectedResult.add(78.0);
+        ArrayList<Double> result = course.getFinalScores();
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testCalcStudentAvg4() {
+        Course course = new Course("Programming1", 4, new Department("CST"));
+
+        course.calcStudentsAverage();
+
+        ArrayList<Double> expectedResult = new ArrayList<>();
+        ArrayList<Double> result = course.getFinalScores();
+
+        Assertions.assertEquals(expectedResult, result);
     }
 }
