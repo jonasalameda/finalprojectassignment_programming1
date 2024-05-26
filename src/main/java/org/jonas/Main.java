e4fa08f85d64fb9c780c7526f7537192296ea8e6
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-26 18:58:33 -0400

    Add testing field for most classes in Main.java

--- src/main/java/org/jonas/Main.java
+++ src/main/java/org/jonas/Main.java
@@ -2,6 +2,14 @@ package org.jonas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Course course = new Course("progrAmming1", 2, new Department("CST"));
+        course.addAssignment("AssIgnment1", 0.2, 100);
+        course.addAssignment("AssiGnment2", 0.2, 100);
+        course.addAssignment("AssignMENt3", 0.6, 100);
+        Student student = new Student("Dog", Gender.MALE, new Address(5, "x","x","x","x","H8N 3B3"), new Department("Cst"));
+        student.registerCourse(course);
+        Student student2 = new Student("Cat", Gender.MALE, new Address(5, "x","x","x","x","H8N2L4"), new Department("Cst"));
+        student2.registerCourse(course);
+        System.out.println(student);
     }
 }
