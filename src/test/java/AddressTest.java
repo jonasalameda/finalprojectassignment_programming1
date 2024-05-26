4fe39b1087a30cf3f6e756852495d12e98b3caf3
jonas <125613982+jonasalameda@users.noreply.github.com>
2024-05-26 18:24:46 -0400

    Add AddressTest.java unit testing

--- src/test/java/AddressTest.java
+++ src/test/java/AddressTest.java
@@ -1,20 +1,37 @@
+import org.jonas.Address;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AddressTest {
     @Test
     public void testIsPostalCodeValid1() {
+        boolean expectedResult = true;
+        boolean result = Address.isPostalCodeValid("H8N 3B3");
 
+        Assertions.assertEquals(expectedResult, result);
     }
     @Test
     public void testIsPostalCodeValid2() {
+        boolean expectedResult = true;
+        boolean result = Address.isPostalCodeValid("h8n3b3");
 
+        Assertions.assertEquals(expectedResult, result);
     }
     @Test
     public void testIsPostalCodeValid3() {
+        boolean expectedResult = false;
+        boolean result = Address.isPostalCodeValid("A3U4E83");
 
+        Assertions.assertEquals(expectedResult, result);
     }
     @Test
     public void testIsPostalCodeValid4() {
-
+        boolean expectedResult = false;
+        boolean result = Address.isPostalCodeValid(null);
+    }
+    @Test
+    public void testIsPostalCodeValid5() {
+        boolean expectedResult = false;
+        boolean result = Address.isPostalCodeValid("");
     }
 }
